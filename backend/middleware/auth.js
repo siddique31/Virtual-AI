b64531a75b23171d61e75acb8375186ddb71da9f
siddique31 <hafizsiddique531@gmail.com>
2026-05-24 22:50:02 +0500

    auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
+module.exports = async (req, res, next) => {
+  try {
+    let token
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+      token = req.headers.authorization.split(' ')[1]
+    }
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        error: 'Not authorized, no token'
+      })
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = await User.findById(decoded.id).select('-password')
+
+    if (!req.user) {
+      return res.status(401).json({
+        success: false,
+        error: 'User not found'
+      })
+    }
+
+    next()
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      error: 'Not authorized'
+    })
+  }
+}
